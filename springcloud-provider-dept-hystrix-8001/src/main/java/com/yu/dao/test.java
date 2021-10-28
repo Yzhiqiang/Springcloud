e4bac0a0cd49089638498ff1ee311bee3697d20c
Yzhiqiang <2595889669@qq.com>
2021-10-28 18:28:23 +0800

    Springcloud学习

---
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/yu/dao/test.java
@@ -0,0 +1,13 @@
+package com.yu.dao;
+
+/**
+ * @Author:Yuzhiqiang
+ * @Description:
+ * @Date: Create in 18:27 2021/10/28
+ * @Modified By:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
